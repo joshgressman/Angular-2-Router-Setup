4d9a0c3ee5840f39792fd28ed7d1a0ef5e548300
joshgressman <joshgressman@gmail.com>
2017-06-08 13:25:07 -0500

    added roting module

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -10,7 +10,7 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() { //getting dats from the activated route
     const id = +this.route.snapshot.params['id'];
@@ -23,6 +23,9 @@ export class ServerComponent implements OnInit {
     ); //listens for params changes
   }
 
-
+ onEdit(){
+  this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
+ }
+//navigate on click
 
 }
